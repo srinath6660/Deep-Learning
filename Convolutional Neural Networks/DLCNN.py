46808184d6f8c978fdad2b6f0e8da0267ab5f7ca
srinath6660 <62426826+srinath6660@users.noreply.github.com>
2020-04-26 19:27:24 +0530

    Add files via upload

---
+++ Convolutional Neural Networks/DLCNN.py
@@ -0,0 +1,78 @@
+# Building the CNN
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
+# Initializing the CNN
+classifier = Sequential()
+
+# Step1 : Convolution
+classifier.add(Convolution2D(32, 3, 3, input_shape = (64, 64, 3), activation = 'relu')) # 32 feature detectors and 32 feature maps
+
+# Step2 : Pooling
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Adding 2nd covolution layer
+classifier.add(Convolution2D(32, 3, 3, activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Step3 : Flattening 
+classifier.add(Flatten())
+
+# Step4 : Full Connection
+classifier.add(Dense(output_dim = 128, activation = 'relu')) # Hidden Layer
+classifier.add(Dense(output_dim = 1, activation = 'sigmoid')) # Output Layer
+
+# Compiling the CNN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Fitting the CNN to our images
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+training_set = train_datagen.flow_from_directory(
+        'dataset/training_set',
+        target_size=(64, 64),
+        batch_size=32,
+        class_mode='binary')
+
+test_set = test_datagen.flow_from_directory(
+        'dataset/test_set',
+        target_size=(64, 64),
+        batch_size=32,
+        class_mode='binary')
+
+classifier.fit_generator(
+        training_set,
+        steps_per_epoch=8000,
+        epochs=25,
+        validation_data=test_set,
+        validation_steps=2000)
+
+
+import numpy as np
+from keras.preprocessing import image
+test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size = (64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = classifier.predict(test_image)
+training_set.class_indices
+if result[0][0] == 1:
+    prediction = 'dog'
+else:
+    prediction = 'cat'
+
+
+
+
+
+
